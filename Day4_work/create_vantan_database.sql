035582ea3e3bebdeed255a3aaffc2afc8c0b7ec0
takashi823 <uchimura.takashi724@gmail.com>
2023-11-06 12:13:39 +0900

    add:sqlのコメント追加

--- Day4_work/create_vantan_database.sql
+++ Day4_work/create_vantan_database.sql
@@ -1,6 +1,11 @@
+-- vantanデータベースを作成
 create database if not exists vantan;
+
+-- 作成したvantan データベースを選択
 use vantan;
-create table if not exsits uses ( 
+
+-- usersテーブルを作成
+create table if not exsits users ( 
     id INT auto_increment, 
     name varchar(20), 
     age INT, 
@@ -8,13 +13,21 @@ create table if not exsits uses (
     primary key (id) 
 );
 
+-- usersテーブルへのデータの追加
 insert into users (name, age) value ('sample1', 1);
 insert into users (name, age) value ('sample2', 2);
 insert into users (name, age) value ('sample3', 3);
 insert into users (name, age) value ('sample4', 4);
 insert into users (name, age) value ('sample5', 5);
 
+-- usersテーブルのレコード全て表示
 select * from users;
+
+-- usersテーブルの設定を確認
 show create table users;
+
+-- vantan データベースのテーブル一覧を表示
 show tables;
+
+-- データベース一覧を表示
 show databases;
